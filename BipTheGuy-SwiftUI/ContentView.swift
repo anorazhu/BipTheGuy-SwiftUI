c3866009c4ed7387b90e5a1c498f3163faffeee5
anorazhu <zhusm@bc.edu>
2024-09-22 14:38:59 -0400

    3.3 PhotosPicker in SwiftUI

--- BipTheGuy-SwiftUI/ContentView.swift
+++ BipTheGuy-SwiftUI/ContentView.swift
@@ -7,28 +7,50 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
     
     var body: some View {
         VStack {
             Spacer()
             
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
                 .onTapGesture {
                     playSound(soundName: "punchSound")
+                    animateImage = false // will immediately shrrink using .scaleEffect tp 90% of size
+                    withAnimation (.spring(response: 0.3, dampingFraction: 0.3)) {
+                        animateImage = true // will go from 90% size to 100% size but sing the .spring animation
+                    }
                 }
+                
             
             Spacer()
             
-            Button {
-                //TODO: Button Action here
-            } label: {
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
             }
+            onChange(of: selectedPhoto) {
+                Task {
+                    do {
+                        if let data = try await selectedPhoto?.loadTransferable(type: Data.self) {
+                            if let uiImage = UIImage(data: data) {
+                                bipImage = Image(uiImage: uiImage)
+                            }
+                        }
+                    } catch {
+                        print("😡 ERROR: loading failed \(error.localizedDescription)")
+                    }
+                }
+            }
+
 
         }
         .padding()
@@ -36,7 +58,7 @@ struct ContentView: View {
 
     func playSound(soundName: String) {
         guard let soundFile = NSDataAsset(name:soundName) else {
-            print("Could not read file name \(soundName)")
+            print("😡 Could not read file name \(soundName)")
             return()
         }
         
@@ -44,7 +66,7 @@ struct ContentView: View {
             audioPlayer = try AVAudioPlayer(data: soundFile.data)
             audioPlayer.play()
         } catch {
-            print("ERROR: \(error.localizedDescription) creating audioPlayer.")
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer.")
         }
     }
 }

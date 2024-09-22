3f67e6fbf11fd53cdb3afc7a06083c09e2f61d33
anorazhu <zhusm@bc.edu>
2024-09-22 13:55:34 -0400

    Initial Commit

---
+++ BipTheGuy-SwiftUI/BipTheGuy_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuy_SwiftUIApp.swift
+//  BipTheGuy-SwiftUI
+//
+//  Created by Anora Zhu on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuy_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BipTheGuy-SwiftUITests/BipTheGuy_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuy_SwiftUITests.swift
+//  BipTheGuy-SwiftUITests
+//
+//  Created by Anora Zhu on 9/22/24.
+//
+
+import Testing
+@testable import BipTheGuy_SwiftUI
+
+struct BipTheGuy_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
